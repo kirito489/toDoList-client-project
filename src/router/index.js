7256f771c066b826771c76b62c58de91309c9545
Rean Tsai <aobc55625@gmail.com>
2025-08-21 19:00:58 +0800

    feat: add route configuration with authentication guard

--- src/router/index.js
+++ src/router/index.js
@@ -1,8 +1,19 @@
 import { createRouter, createWebHistory } from 'vue-router'
 
+import Home from '../views/HomeView.vue'
+import Auth from '../views/AuthView.vue'
+import Todos from '../views/TodosView.vue'
+
+const routes = [
+  { path: '/', redirect: '/home' },
+  { path: '/home', name: 'Home', component: Home },
+  { path: '/auth', name: 'Auth', component: Auth },
+  { path: '/todos', name: 'Todos', component: Todos, meta: { requiresAuth: true } },
+]
+
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
-  routes: [],
+  routes: routes,
 })
 
 export default router
